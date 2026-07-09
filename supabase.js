18836ebf6e8717f1679d5515f92c1dc4061b02f1
totuflatkokur <totuflatkokur@totuflatkokur.is>
2026-07-09 22:53:09 +0000

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,8 +1,8 @@
 // Tótu Stimpilklukka v2
-// Settu publishable key-inn þinn hér fyrir neðan.
 
 const SUPABASE_URL = "https://rzvcprkdriiywupaqupv.supabase.co";
-const SUPABASE_ANON_KEY = "SETTU_PUBLISHABLE_KEY_HER";
+
+const SUPABASE_ANON_KEY = "sb_publishable_jRtzeX7p1fweD9NMAPaRzQ_p0LCFYq5";
 
 const db = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
